219f158a5894ca2014deb33c15d12260c8eca0c1
VALTECH-COM\pedro.sousa <pedro.sousa@valtech.com>
2025-08-08 14:19:58 +0100

    Fixed Swagger

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
+using System.Net;
 
 namespace My.Function;
 
@@ -15,6 +18,8 @@ public class HttpTrigger1
     }
 
     [Function("HttpTrigger1")]
+    [OpenApiOperation(operationId: "GetWelcomeMessage", tags: new[] { "greeting" }, Summary = "Get welcome message", Description = "This function returns a welcome message.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "This returns the response")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
